383b1aeb091fb6db3027431c2fc4e51473c39536
arham777 <arhamawan200@gmail.com>
2024-10-02 08:21:55 +0500

    new file

---
+++ lab4.dart
@@ -0,0 +1,207 @@
+// LIST
+
+// // 1) How do you declare an empty list in Dart?
+
+// void main(){
+//   List lst = List.filled(4, null);
+// }
+
+
+
+
+// // 2) How do you access elements in a Dart list?
+
+// void main(){
+//   List lst = List.filled(4, null);
+//   print(lst);
+// }
+
+
+
+// // 3) Explain how to add an element to the end of a Dart list
+
+// void main() {
+//  List tst = [ 'One', 'Two' , 'Three' ]  ;
+//  print(tst);
+//  print(tst[0]);
+  
+//   tst.add( 'Four' );
+//   print(tst);
+  
+// }
+
+
+
+// // 4) How do you remove an element from a specific index in a Dart list?
+// void main() {
+//  List tst = [ 'One', 'Two' , 'Three' ]  ;
+//  print(tst);
+ 
+//   tst.removeAt(1);
+ 
+//    print(tst);
+// }
+
+
+
+
+// // 5) What is the length property of a Dart list?
+
+// void main() {
+//  List tst = [ 'One', 'Two' , 'Three' ]  ;
+//  print(tst);
+ 
+//   print(tst.length);
+// }
+
+
+
+
+
+// MAP
+
+// // 1) How do you declare an empty list in Dart?
+
+
+//  import 'dart:io';
+// void main() {
+//  dynamic mapType = {};
+
+//  print(mapType);
+//  }
+
+
+
+
+// // 2) Provide an example of initializing a map with key-value pairs
+
+//  import 'dart:io';
+// void main() {
+//  dynamic mapType = new Map();
+//  mapType['First'] = 'University';
+//  mapType['Second'] = 'of';
+//  mapType['Third'] = 'Lahore';
+//  print(mapType);
+//  }
+
+
+
+
+
+// // 3) How do you access elements in a Dart list?
+
+//  import 'dart:io';
+// void main() {
+//  dynamic mapType = new Map();
+//  mapType['First'] = 'University';
+//  mapType['Second'] = 'of';
+//  mapType['Third'] = 'Lahore';
+//  print(mapType);
+//  }
+
+
+
+
+
+// // 4) Explain how to add a new key-value pair to a Dart map.
+
+// import 'dart:io';
+
+// void main() {
+//   dynamic mapType = new Map();
+//   mapType['First'] = 'University';
+//   mapType['Second'] = 'of';
+//   mapType['Third'] = 'Lahore';
+//   print(mapType);
+//   mapType['New'] = 'Addition';
+//   print(mapType);
+// }
+
+
+
+
+// \\ 5) What is the length property of a Dart map?
+
+// import 'dart:io';
+
+// void main() {
+//   dynamic mapType = new Map();
+//   mapType['First'] = 'University';
+//   mapType['Second'] = 'of';
+//   mapType['Third'] = 'Lahore';
+//   print(mapType);
+    
+//   print(mapType.length);
+// }
+
+
+
+// VARIABLES
+
+
+// // 1) Declare two variables, a and b, with values 15 and 7 respectively. Print their sum.
+
+// void main() {
+//  var a= 15;
+//   var b = 7;
+//   var c= a+b;
+//   print(c);
+// }
+
+
+// // 2) Declare two variables, x and y, with values 10.5 and 3.2 respectively. Print their 
+// // product.
+
+
+// void main() {
+//  var x= 10.5;
+//   var y = 3.2;
+//   var c= x*y;
+//   print(c);
+// }
+
+
+
+
+
+// // 3) Declare a variable num with a value of -8. Print its absolute value
+
+
+// void main() {
+//  var num = -8;
+//   print(num.abs());
+// }
+
+
+
+// // 4) Declare a variable decimalNum with a value of 7.3. Print its ceiling and floor values.
+// void main() {
+  
+//   double decimalNum = 7.3;
+//   int ceilingValue = decimalNum.ceil();
+//   int floorValue = decimalNum.floor();
+
+//   print('The ceiling value of $decimalNum is: $ceilingValue'); 
+//   print('The floor value of $decimalNum is: $floorValue'); 
+// }
+
+
+
+// // 5) Compare the values of a and b. Print whether a is greater than b.
+
+// void main() {
+//  var a = 8;
+//   var b= 12;
+//   if (a > b) {
+//     print('a ($a) is greater than b ($b)');
+//   } else if (a < b) {
+//     print('a ($a) is not greater than b ($b)');
+//   } else {
+//     print('a ($a) is equal to b ($b)');
+//   }
+// }
+
+
+
+
+
---
+++ week 1/lab_2/lab2/bin/lab2.dart
@@ -0,0 +1,205 @@
+
+
+// // Q1:
+// int main(){
+
+// int num1 = 18;
+// int num2= 2;
+// double c= num1/num2;
+// print(c);
+
+//   return 0;
+// }
+
+
+
+
+// // Q2:
+// int main(){
+
+// int a = 12;
+// var b= -a;
+// print(b);
+
+//   return 0;
+// }
+
+
+
+
+
+// // Q3:
+// int main(){
+
+// var a = 12;
+// var b = 18;
+// var c = a<=b;
+// print(c);
+
+// a= 13;
+// b=6;
+// c= a<=b;
+// print(c);
+
+//   return 0;
+// }
+
+
+
+
+// // Q4:
+// int main(){
+
+// var a = 12;
+// var b = 12;
+// var c = a==b;
+// print(c);
+
+// a= 13;
+// b=6;
+// c= a==b;
+// print(c);
+
+//   return 0;
+// }
+
+
+
+
+// // Q5:
+// int main(){
+
+// var a = 12;
+// var b = 12;
+// var c = a!=b;
+// print(c);
+
+// a= 13;
+// b=6;
+// c= a!=b;
+// print(c);
+
+//   return 0;
+// }
+
+
+
+
+// // Q6:
+
+// int main(){
+// var a;
+// a ??=12;
+// print(a);
+
+// a??=18;
+// print(a);
+
+//   return 0;
+// }
+
+
+// Q7:
+// void main(){
+
+// var age= 21;
+// var city= "Lahore";
+// var check = (age>= 18 && city=="Lahore" )? "Person is eligible to vote" : "Person is not eligible to vote";
+// print(check);
+
+// age= 16;
+// check = (age>= 18 && city=="Lahore" )? "Person is eligible to vote" : "Person is not eligible to vote";
+// print(check);
+// }
+
+
+//  Q8:
+// void main(){
+
+// var amount= 122;
+// var member= 'premium';
+// var check = (amount>= 100 && member=='premium' )? "Person is eligible for a dicount" : "Person is not eligible for a dicount";
+// print(check);
+
+// member='';
+// check = (amount>= 100 && member=='premium' )? "Person is eligible for a dicount" : "Person is not eligible for a dicount";
+// print(check);
+// }
+
+
+// Q9:
+// void main(){
+
+// var user= 'sub';
+// var check = ( user!='sub' )? "Recieves promotional offer" : "Does not recieve promotional offer";
+// print(check);
+
+// user='';
+// check = ( user!='sub' )? "Recieves promotional offer" : "Does not recieve promotional offer";
+// print(check);
+// }
+
+
+// Q10:
+// void main(){
+
+// var num= 12;
+// var check = ( num>=0 )? "Number is positive" : "Number is negative";
+// print(check);
+
+// num= -12;
+// check = ( num>=0 )? "Number is positive" : "Number is negative";
+// print(check);
+
+// }
+
+
+// Q11:
+// void main(){
+
+// var num= 12;
+// var check = ( num%2==0 )? "Number is even" : "Number is odd";
+// print(check);
+
+// num= 3;
+// check = ( num%2==0 )? "Number is even" : "Number is odd";
+// print(check);
+
+// }
+
+// Q12:
+// void main(){
+
+// var num= 12;
+// var check = ( num%2==0 )? "Number is even" : "Number is odd";
+// print(check);
+
+// num= 3;
+// check = ( num%2==0 )? "Number is even" : "Number is odd";
+// print(check);
+
+// }
+
+
+// Q12:
+// void main(){
+
+// var age= 21;
+// var city= "Lahore";
+// var check = (age>= 18 && city=="Lahore" )? "Person is eligible to vote" : "Person is not eligible to vote";
+// print(check);
+
+// }
+
+
+//  Q13:
+// void main(){
+
+// var amount= 122;
+// var member= 'premium';
+// var check = (amount>= 100 && member=='premium' )? "Person is eligible for a dicount" : "Person is not eligible for a dicount";
+// print(check);
+
+// }
+
+
