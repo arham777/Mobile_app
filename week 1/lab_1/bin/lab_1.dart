ba2cfab204b4a1e1cbd97c7acf80d1e64fa8986f
arham777 <arhamawan200@gmail.com>
2024-10-02 23:21:25 +0500

    lab1 re-added

---
+++ week 1/lab_1/bin/lab_1.dart
@@ -0,0 +1,29 @@
+// import 'package:lab_1/lab_1.dart' as lab_1;
+
+// void main(List<String> arguments) {
+//   print( "\$" 'Hello world: ${lab_1.calculate()}!');
+// }
+
+
+void main(){
+  
+  int num = 15;
+  print(num);
+
+  double newVar= 10.5;
+  print(newVar);
+  
+  var newNum= 12;
+  print(newNum);
+
+  const newValue= 13.0321;
+  print(newValue);
+
+  final checkTime= DateTime.now();
+  print(checkTime);
+
+  dynamic value= 1001.22;
+  print(value);
+
+
+}
