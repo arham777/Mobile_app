63c54bb4d0f26420178fe20ef9ab9be6ee08f415
arham777 <arhamawan200@gmail.com>
2024-10-29 09:56:49 +0500

    quiz1

---
+++ Quizes/quiz1/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 19, 19, 19),
+        appBar: AppBar(
+          title: const Text("abc"),
+        ),
+        body: 
+         const Center(
+          //  child: Text('Currency Converter',
+          //          style: TextStyle(
+          //     color: Colors.white,
+          //   ),
+          //          ),
+
+              // child: TextField( 
+              //     decoration: InputDecoration(
+              //       hintText: 'Enter the amount',
+              //       hintStyle: TextStyle(
+              //         color: Colors.white,
+              //       ),
+              //       focusedBorder: OutlineInputBorder(
+              //         borderRadius: BorderRadius.all(Radius.circular(25)),
+              //         borderSide: BorderSide(color: Colors.white),
+              //       ),
+              //       enabledBorder: OutlineInputBorder(
+              //         borderRadius: BorderRadius.all(Radius.circular(25)),
+              //         borderSide: BorderSide(color: Color.fromARGB(255, 18, 253, 194)),
+              //       ),
+              //       // border: OutlineInputBorder(
+              //       //   borderRadius: BorderRadius.all(Radius.circular(25)),
+              //       //   borderSide: BorderSide(color: Colors.white),
+              //       // ),
+              //     ),
+              //   ),
+              
+              // ),
+            
+        //  ),
+          
+       ),
+
+          TextButton(onPressed: () { 
+          debugPrint( 'pressed');
+          
+        }, 
+        
+
+       ),
+      
+    ),
+    );
+
+  }
+
+}
